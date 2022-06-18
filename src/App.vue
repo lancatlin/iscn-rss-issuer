8eae93a457e632e11b47d218913a0e1657c6c65e
wancat <wancat@wancat.cc>
2022-06-18 11:04:02 +0800

    setup Vue project

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h1>ISCN RSS Issuer</h1>
+        <input type="text" v-model="rssLink" name="rssLink" placeholder="Input the RSS link...">
+        <p>{{ rssLink }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            rssLink: "",
+        }
+    }
+}
+</script>
+
